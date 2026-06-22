5b93a9a9c92f06f9f48a0234a9c9da0c00431d7a
Bakri M <bakrimalahefji@gmail.com>
2026-06-23 01:05:01 +0200

    cursor animations

--- init.lua
+++ init.lua
@@ -421,6 +421,21 @@ do
   --  - va)  - [V]isually select [A]round [)]paren
   --  - yiiq - [Y]ank [I]nside [I]+1 [Q]uote
   --  - ci'  - [C]hange [I]nside [']quote
+  vim.pack.add({
+    "https://github.com/sphamba/smear-cursor.nvim",
+  })
+
+  require('smear_cursor').setup({
+  opts = {                                -- Default  Range
+    stiffness = 0.8,                      -- 0.6      [0, 1]
+    trailing_stiffness = 0.6,             -- 0.45     [0, 1]
+    stiffness_insert_mode = 0.7,          -- 0.5      [0, 1]
+    trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+    damping = 0.95,                       -- 0.85     [0, 1]
+    damping_insert_mode = 0.95,           -- 0.9      [0, 1]
+    distance_stop_animating = 0.5,        -- 0.1      > 0
+  },
+  })
   require('mini.ai').setup {
     -- NOTE: Avoid conflicts with the built-in incremental selection mappings on Neovim>=0.12 (see `:help treesitter-incremental-selection`)
     mappings = {
